4e712a6751e9478cc09dd0fbecb8553cecf9d98c
Amogh Warkhandkar <amoghw2025@gmail.com>
2021-02-28 22:59:07 +0530

    Added Model Training Notebook

---
+++ notebooks/dataset/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps.zip
+wget https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps-test-set.zip
+7z x rps.zip
+7z x rps-test-set.zip
